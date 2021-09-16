6b50a36232f80492bc8f3bb686c909aba971cb93
FearlessIIX <84805129+FearlessIIX@users.noreply.github.com>
2021-09-15 20:50:20 -0400

    Create runawayElement.svelte

---
+++ David/runawayElement.svelte
@@ -0,0 +1,31 @@
+<script>
+	let m = { x: 0, y: 0 };
+	let v = false
+
+	function handleMousemove(event) {
+		m.x = event.clientX;
+		m.y = event.clientY;
+	}
+</script>
+
+<div on:mousemove={handleMousemove} >
+	{#if v}
+	<br><br><br><br><br><br><br><br><br><br><br>
+	<p on:mouseover="{e => v = !v}" on:focus>
+		The mouse position is {m.x} x {m.y}
+	</p>
+	
+	{:else}
+	<p on:mouseover="{e => v = !v}" on:focus>
+		The mouse position is {m.x} x {m.y}
+	</p>
+	
+	{/if}
+</div>
+
+<style>
+	div { width: 100%; height: 100%; }
+	p {
+		padding: 20px;
+	}
+</style>
