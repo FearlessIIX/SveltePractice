0c2e9e0a316ea931ac07ee8787bd49f37004526b
FearlessIIX <84805129+FearlessIIX@users.noreply.github.com>
2021-10-01 13:49:04 -0400

    Create index.svelte

---
+++ David/more-stuff/index.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Button from "./Button.svelte";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div id="thing">
+    <Button></Button><Button></Button><Button></Button>
+</div>
+<style>
+    #thing {
+        display:flex;
+        justify-content: space-between;
+        width: 75vw;
+        left:12.5vw;
+        position: absolute;
+    }
+</style>
