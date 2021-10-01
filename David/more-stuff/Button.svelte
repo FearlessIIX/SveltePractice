f660ed93a787e7f727b4a819a132358751a79638
FearlessIIX <84805129+FearlessIIX@users.noreply.github.com>
2021-10-01 13:48:49 -0400

    Create Button.svelte

---
+++ David/more-stuff/Button.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { onClick } from "./Scripts/btnStuff";
+</script>
+<button class="btn" on:click="{onClick}">
+    Press Me
+</button>
+
+<style>
+    .btn {
+        border: black solid 2px;
+        background-color: gray;
+        padding: 15px;
+        border-radius: 15px;
+        color:black;
+        transition: background-color 0.4s, color 0.4s;
+
+    }
+    
+    .btn:hover {
+        background-color: lightgrey;
+        color:rgb(100, 100, 100);
+    }
+</style>
