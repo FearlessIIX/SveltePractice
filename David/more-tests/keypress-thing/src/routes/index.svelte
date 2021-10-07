5045196de88ab9d6e626f67ff5c6f93026dc9e46
FearlessIIX <84805129+FearlessIIX@users.noreply.github.com>
2021-10-06 22:19:33 -0400

    Add files via upload

---
+++ David/more-tests/keypress-thing/src/routes/index.svelte
@@ -0,0 +1,35 @@
+<script>
+    let keyPressed = "";
+    var contents = [""];
+    function keyup() { keyPressed = ""; }
+    function keydown(event) {
+        // First Section
+        if(event.key == " ") keyPressed = "space";
+        else keyPressed = event.key;
+        // Second Section
+        if (event.key == "Shift" || event.key == "Backspace" || event.key == "Enter") {
+            if (event.key == "Backspace") {
+                contents = contents.slice(0, contents.length - 1);
+            }
+            else if (event.key == "Enter") {
+                //TODO: add functionality for the enter button
+                //perhaps some kind of line break or something??
+            }
+        }
+        else contents += event.key;
+    }
+</script>
+<svelte:window on:keyup={keyup} on:keydown={keydown}></svelte:window>
+<div id="top" class="bg-gray-300 h-64 w-screen items-center flex justify-center">
+    <div class="key-box">
+        {keyPressed}
+    </div>
+</div>
+<div id="bottom" class="bg-gray-300 h-64 w-screen items-center flex justify-center">
+    <div class="text-box ">
+        {contents}
+    </div>
+</div> 
+<style>
+    @import "./style.css";
+</style>
