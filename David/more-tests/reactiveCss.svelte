2be34069c56537c3f00d44c26c329faaa1af3601
FearlessIIX <84805129+FearlessIIX@users.noreply.github.com>
2021-10-08 00:28:32 -0400

    made reactive css. ? . . Kinda
    An attempt to use svelte javascript variables to make my css reactive

---
+++ David/more-tests/reactiveCss.svelte
@@ -0,0 +1,19 @@
+<script>
+	let styleOf = "red"
+	function click() {
+		if (styleOf == "red") {
+			styleOf = "blue";
+		}
+		else styleOf = "red";
+	}
+</script>
+<svelte:window on:click={click}/>
+<p id="transition" class={styleOf}>
+	This is text
+</p>
+
+<style>
+	.red { color:red; }
+	.blue { color:blue; }
+	p { transition:color 2s; }
+</style>
