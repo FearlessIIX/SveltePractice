e29b5e3fd99cc27e03127a2033e5d237b297b467
FearlessIIX <84805129+FearlessIIX@users.noreply.github.com>
2021-10-09 16:31:15 -0400

    Update index.svelte

--- David/reactive/topBar/index.svelte
+++ David/reactive/topBar/index.svelte
@@ -4,21 +4,39 @@
 		visibility: "hide"
 	}
 	let theme = "Light";
+	function alertTest() {
+		var text = (themeDropdown.status == true) ? 
+			"You know what I meant!!" : "Button Clicked!!";
+		alert(text);
+	}
+	function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
 	
-	function toggleDropdown() {
-		themeDropdown.status = !themeDropdown.status;
+	async function toggleDropdown() {
 		if (themeDropdown.visibility == "hide") {
 			themeDropdown.visibility = "show"
+			themeDropdown.status = true;
 		}
-		else themeDropdown.visibility = "hide"
+		else {
+			themeDropdown.visibility = "hide";-
+			await sleep(1500);
+			if (!(themeDropdown.visibility == "show")) {
+				themeDropdown.status = false;
+			}
+		}
 	}
 	function toggleTheme() {
-		if (theme == "Light") theme = "Dark"
-		else theme = "Light";
+		if (themeDropdown.visibility == "show") {
+			if (theme == "Light") theme = "Dark"
+			else {
+				theme = "Light";
+			}
+		}
 	}
 </script>
 <div id="page">
-	<div class="sticky">
+	<div class="sticky {theme}">
 <!-- 	Navigation Bar	 -->
 		<div class="navbar {theme}">
 			<div class="button {theme}">
@@ -35,8 +53,8 @@
 			</div>
 		</div>
 <!-- 	Theme Selection box	 -->
-		<!-- {#if themeDropdown.visibility == "show"} -->
 		<div class="j-right {themeDropdown.visibility}">
+			{#if themeDropdown.status}
 			<div class="theme-selector {theme}">
 				<h4>Theme</h4>
 				<div id="t-buttons">
@@ -45,8 +63,20 @@
 					</div>
 				</div>
 			</div>
+			{/if}
 		</div>
-		<!-- {/if} -->
+	</div>
+	<div class="page-content {theme}">
+		<div>
+			<button class="btn-test {theme}" on:click={alertTest}>A clickable button</button>
+			<p class="arrow flex float-right m-7">This button is able to be pressed<br>
+				when the theme selector is hidden</p>
+			<p class="flex float-right m-7">Normally these elements interfere with<br>
+				the rest of the page when used incorrectly</p>
+			<p class="flex float-right m-7">The theme selector is an absolute<br>
+				positoned element, meaning it goes<br>
+				over top of the other elements</p>
+		</div>
 	</div>
 </div>
 <style>
