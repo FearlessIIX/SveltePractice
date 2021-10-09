24bb632865541c5528b7dd12841e24196f249035
FearlessIIX <84805129+FearlessIIX@users.noreply.github.com>
2021-10-08 21:06:36 -0400

    Create index.svelte

---
+++ David/reactive/topBar/index.svelte
@@ -0,0 +1,56 @@
+<script>
+	let themeDropdown = {
+		status: false,
+		visibility: "hidden"
+	}
+	let theme = "Light";
+	
+	function toggleDropdown() {
+		themeDropdown.status = !themeDropdown.status;
+		if (themeDropdown.visibility == "hide") {
+			themeDropdown.visibility = "show"
+		}
+		else themeDropdown.visibility = "hide"
+	}
+	function toggleTheme() {
+		if (theme == "Light") theme = "Dark"
+		else theme = "Light";
+	}
+</script>
+<div id="page">
+	<div class="sticky">
+<!-- 	Navigation Bar	 -->
+		<div class="navbar {theme}">
+			<div class="button {theme}">
+				item
+			</div>
+			<div class="button {theme}">
+				item
+			</div>
+			<div class="button {theme}">
+				item
+			</div>
+			<div class="button {theme}" on:click={toggleDropdown}>
+				Theme
+			</div>
+		</div>
+<!-- 	Theme Selection box	 -->
+		<!-- {#if themeDropdown.visibility == "show"} -->
+		<div class="j-right {themeDropdown.visibility}">
+			<div class="theme-selector {theme}">
+				<h4>Theme</h4>
+				<div id="t-buttons">
+					<div class="t-button {theme}" on:click={toggleTheme}>
+                        {theme}
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- {/if} -->
+	</div>
+</div>
+<style>
+    @import "./style.css";
+    @import "./themes/light.css";
+    @import "./themes/dark.css";
+</style>
